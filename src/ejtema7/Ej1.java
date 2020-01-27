287d227bdbbc1942d22e4005d1c518571495d117
Javi <javielcartas@gmail.com>
2020-01-27 21:21:54 +0100

    Hola, este es un proyecto

--- src/ejtema7/Ej1.java
+++ src/ejtema7/Ej1.java
@@ -20,8 +20,8 @@ public class Ej1 {
         num[1]=-2;
         num[4]=0;
         num[6]=14;
-        num[8]=5;
-        num[9]=120;
+        num[8]=50;
+        num[9]=121;
         for (int i = 0; i < 12; i++) {
             System.out.println(num[i]);
             
